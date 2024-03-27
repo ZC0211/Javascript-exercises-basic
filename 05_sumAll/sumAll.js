8c7b2cf3dad570d489ddc33b0d3d8f6299b054b9
zoechen1014 <chen.zuo@northeastern.edu>
2024-03-26 21:28:52 -0700

    Basic exercise from the TOP

---
+++ 05_sumAll/sumAll.js
@@ -0,0 +1,21 @@
+const sumAll = function(num1,num2) {
+    if(typeof num1 === "number" && typeof num2 === "number" && num1 > 0 && num2 > 0){
+        let result = 0;
+        if(num1 < num2){
+            for (let i = num1; i <= num2; i++){
+                result += i;
+            }
+            return result;
+        } else if (num2 < num1){
+            for (let i = num2; i <= num1; i++){
+                result += i;
+            }
+            return result;
+        }
+    } else {
+        return 'ERROR';
+    }
+};
+
+// Do not edit below this line
+module.exports = sumAll;
---
+++ 07_tempConversion/tempConversion.js
@@ -0,0 +1,17 @@
+const convertToCelsius = function(temperature) {
+  let result = ((temperature - 32) * (5/9));
+  result = Math.round(result * 10) / 10;
+  return result;
+};
+
+const convertToFahrenheit = function(temperature) {
+  let result = ((temperature * (9/5)) + 32);
+  result = Math.round(result * 10) / 10;
+  return result;
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
