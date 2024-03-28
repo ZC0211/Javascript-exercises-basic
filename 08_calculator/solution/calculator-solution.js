4efc70e7250a844ad32dee6ed6a65b6ebcef350b
zoechen1014 <chen.zuo@northeastern.edu>
2024-03-27 18:37:14 -0700

    Update 08 to 12

--- 08_calculator/solution/calculator-solution.js
+++ 08_calculator/solution/calculator-solution.js
@@ -7,7 +7,7 @@ const subtract = function (a, b) {
 };
 
 const sum = function (array) {
-  return array.reduce((total, current) => total + current, 0);
+  return array.reduce((total, current) => total + current);
 };
 
 const multiply = function (array) {
